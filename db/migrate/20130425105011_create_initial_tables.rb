92f1d661ecbcf86a1626569a9d1fa994113a2dc7
Apprentice <apprentice@devbootcamp.com>
2013-04-25 11:19:09 -0700

    add 'not null' validations to database and models

--- db/migrate/20130425105011_create_initial_tables.rb
+++ db/migrate/20130425105011_create_initial_tables.rb
@@ -1,20 +1,20 @@
 class CreateInitialTables < ActiveRecord::Migration
   def change
     create_table :authors do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
       t.timestamps
     end
 
     create_table :posts do |t|
-      t.references :author
-      t.string :title
-      t.text :content
+      t.references :author, :null => false
+      t.string :title, :null => false
+      t.text :content, :null => false
       t.timestamps
     end
 
     create_table :tags do |t|
-      t.string :name
+      t.string :name, :null => false
       t.timestamps
     end
 
